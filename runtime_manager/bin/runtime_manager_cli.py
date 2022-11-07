08866b1fce15c3e26eda0544723dc2d418649e51
bedoya <bedoya@cefriel.it>
2022-11-07 15:37:18 +0100

    case C verifier and tosca exchanger

--- runtime_manager/bin/runtime_manager_cli.py
+++ runtime_manager/bin/runtime_manager_cli.py
@@ -16,10 +16,11 @@ import sys
 import yaml
 from deepdiff import DeepDiff
 import json
+import glob
 
 sys.path.append("../")
 from im_interface import  *
-from utils import im_auth_path_def, im_url_def, place_name, yaml_as_dict, dic_creation
+from utils import *
 
 @click.group()
 def runtime_manager_cli():
@@ -38,7 +39,8 @@ def infras(application_dir, dir_to_save):
         tosca["infid"] = InfId
         tosca = place_name(tosca)
         # tosca_path = "%s/%s" % (dir_to_save, tosca_file_dir) + "/" + tosca["component_name"] + "-" + str(i) + ".yaml"
-        tosca_path = dir_to_save + "/" + tosca["component_name"] + "-" + str(i) + ".yaml"
+        # tosca_path = dir_to_save + "/" + tosca["component_name"] + "-" + str(i) + ".yaml"
+        tosca_path = dir_to_save + "/" + tosca["component_name"] + ".yaml"
         with open(tosca_path, 'w+') as f:
             yaml.safe_dump(tosca, f, indent=2)
         print("DONE. TOSCA files %s has been saved for the InfId %s" % (tosca["component_name"] + "-" + str(i) + ".yaml", InfId))
@@ -46,46 +48,56 @@ def infras(application_dir, dir_to_save):
 
 @click.command()
 @click.option("--application_dir", help="Path to the AI-SPRINT application.", required=True, default=None)
-@click.option("--old_dir", help="Path to read the old toscas", default=None)
+@click.option("--old_dir", help="Path to read the old toscas", default=None, required = True)
 @click.option("--new_dir", help="Path to read the new toscas", default=None)
-def difference(application_dir, old_dir, new_dir):   
-    dic_old = {}
-    dic_new = {}
+def difference(application_dir, old_dir, new_dir):
+    # Processing production files
     production_old_dic = yaml_as_dict("%s/aisprint/deployments/base/production_deployment.yaml" % (application_dir))
     production_new_dic = yaml_as_dict("%s/aisprint/deployments/optimal_deployment/production_deployment.yaml" % (application_dir))
-    dic_old = dic_creation(production_old_dic)
-    dic_new = dic_creation(production_new_dic)
-    ddiff = DeepDiff(dic_old, dic_new, ignore_order=True)
-    #assuing that the files are correctlly configured
-
-    if "dictionary_items_added" in ddiff:
-        print("new cluster addes")
-    elif "dictionary_item_removed" in ddiff:
-        print("cluster deleted")
-    elif "values_changed" in ddiff: 
-        print("a valued has been changed")
-        for item, values in ddiff["values_changed"].items():
-            print(item)
-            print(values)
+    files = glob.glob("%s/*.yaml" % old_dir)
+    production_old_dic["System"]["toscas"] = {}
+    i = 0
+    infras = {}
+    for one_file in files:
+        tosca_old_dic = yaml_as_dict(one_file)
+        production_old_dic["System"]["toscas"][tosca_old_dic["component_name"]] = tosca_old_dic
+        infras[i] = {}
+        if tosca_old_dic["component_name"] in production_old_dic["System"]["Components"]:
+            production_old_dic["System"]["Components"][tosca_old_dic["component_name"]]["infid"] = tosca_old_dic["infid"]
+    files = glob.glob("%s/*.yaml" % new_dir)
+    production_new_dic["System"]["toscas"] = {}
+    for one_file in files:
+        tosca_new_dic = yaml_as_dict(one_file)
+        tosca_new_dic = place_name(tosca_new_dic)
+        production_new_dic["System"]["toscas"][tosca_new_dic["component_name"]] = tosca_new_dic
+    
+    
+    # Make a verification of the case--------------------------------------
+    if len(production_old_dic["System"]["Components"]) == len(production_new_dic["System"]["Components"]):
+        print("the number of clusters are the same")
+        components_same = component_name_verification(production_old_dic["System"]["Components"],production_new_dic["System"]["Components"])    
+        machines_same = infrastructures_verification(production_old_dic["System"]["Components"],production_new_dic["System"]["Components"])
+        if components_same == True and machines_same == True:
+            print("Case: same machines and cluster, it is just to exchange the infrastructures between each component")
+            for components_tosca_old, values_tosca_old in production_old_dic["System"]["toscas"].items():
+                for components_new, values_new in production_new_dic["System"]["Components"].items():
+                    if values_tosca_old["infid"] == values_new["infid"]:
+                        tosca_new = production_new_dic["System"]["toscas"][values_new["name"]]
+                        tosca_old = values_tosca_old
+                        correct_name = values_tosca_old["component_name"]
+                        production_new_dic["System"]["toscas"][values_new["name"]] = mix_toscas(correct_name, production_old_dic["System"]["toscas"], tosca_new)
+        print("DONE exchange the infrastructures of each cluster")
+    elif len(production_old_dic["System"]["Components"]) < len(production_new_dic["System"]["Components"]):
+        print( "increase the number of clusters")
     else:
-        print("nothing has been changed")
-    
-    # print(ddiff)
-    # for items_old, values_old in dic_old.items():
-    #     for items_new, values_new in dic_new.items():
-    #         print(values_old)
-    #         print(values_new)
-    # 
+        print( "decrease the number of clusters")
+    #Save the the production
+    with open("%s/production/production_old.yaml" % old_dir, 'w+') as f:
+            yaml.safe_dump(production_old_dic, f, indent=2)
+    with open("%s/production/production_new.yaml" % new_dir, 'w+') as f:
+            yaml.safe_dump(production_new_dic, f, indent=2)
 
-
-    # # corregir
-    # dic_old["executionLayer"] = production_old_dic["System"]["Components"]["component1"]["executionLayer"]
-    # dic_old["executionLayer"] = production_old_dic['System']['NetworkDomains']['ND1']['ComputationalLayers']['computationalLayer1']['Resources']['resource1']['totalNodes']
     
-    # ddiff = DeepDiff(production_old_dic, production_new_dic, ignore_order=True)
-    # changes = ddiff["values_changed"].items()
-    # print(changes)
-    # print(ddiff["values_changed"][0])
 
 @click.command()
 @click.option("--application_dir", help="Path to the AI-SPRINT application.", required=True, default=None)
--- runtime_manager/utils.py
+++ runtime_manager/utils.py
@@ -35,9 +35,75 @@ def dic_creation(dic):
     i = 0
     for item, values in dic["System"]["Components"].items():
         dic_new[values["name"]] =  { 
-            "componenet_name": values["name"],
+        # dic_new[item] =  { 
+
+            # "component_name": values["name"],
             "exec_layer": values["executionLayer"],
             "resource": values["Containers"]["container1"]["selectedExecutionResources"]
         }
         i += 1
     return dic_new
+def mix_toscas(correct_name, toscas_old, tosca_new):
+    tosca_new["topology_template"]["inputs"] = toscas_old[correct_name]["topology_template"]["inputs"]
+    oscar_service_old = toscas_old[correct_name]["topology_template"]["outputs"]["oscar_service_url"]["value"]["get_attribute"][0] 
+    oscar_service_new = tosca_new["topology_template"]["outputs"]["oscar_service_url"]["value"]["get_attribute"][0] 
+    tosca_new["topology_template"]["node_templates"][oscar_service_new]["properties"]["env_variables"]["KCI"] = toscas_old[correct_name]["topology_template"]["node_templates"][oscar_service_old]["properties"]["env_variables"]["KCI"]
+    if len(tosca_new["topology_template"]["node_templates"][oscar_service_new]["properties"]["output"]) > len(toscas_old[correct_name]["topology_template"]["node_templates"][oscar_service_old]["properties"]["output"]):
+        i  = 0
+        cluster_name = toscas_old[tosca_new["component_name"]]["topology_template"]["inputs"]["cluster_name"]["default"]
+        tosca_new["topology_template"]["node_templates"][oscar_service_new]["properties"]["output"] = toscas_old[tosca_new["component_name"]]["topology_template"]["node_templates"][oscar_service_new]["properties"]["output"]
+        for output in tosca_new["topology_template"]["node_templates"][oscar_service_new]["properties"]["output"]:
+            if output["storage_provider"] != "minio":
+                print("uno")
+                tosca_new["topology_template"]["node_templates"][oscar_service_new]["properties"]["output"][i]["storage_provider"] = "minio.%s" % (cluster_name)
+            i += 1
+        tosca_new["topology_template"]["node_templates"][oscar_service_new]["properties"]["storage_providers"].pop("minio", None)
+        tosca_new["topology_template"]["node_templates"][oscar_service_new]["properties"]["storage_providers"]["minio"] = {
+            cluster_name:{
+                "access_key": "minio",
+                "endpoint": "https://minio.%s.%s" % (cluster_name, tosca_new["topology_template"]["inputs"]["domain_name"]["default"]),
+                "region": "us-east-1",
+                "secret_key": toscas_old[tosca_new["component_name"]]["topology_template"]["inputs"]["minio_password"]["default"],
+                
+            }
+        }
+    return tosca_new
+
+def component_name_verification(dic_old, dic_new):
+    count_components = 0
+    for component_new, value_new in dic_new.items():
+        if component_new in dic_old:
+            if dic_new[component_new]["name"] == dic_old[component_new]["name"]:
+                count_components += 1
+            else:
+                print("The component name on '%s' on the new production does not match with the component name on old production" %(component_new))
+                break
+        else:
+            print("The component '%s' in new production does not exist in old production, check the component assignation" % (component_new))
+            break
+    if count_components == len(dic_old):
+        components_same = True
+        print("all Components have the same name")
+    else:
+        components_same = False
+        print("Not all the components have the same name")
+    return components_same
+
+def infrastructures_verification(dic_old, dic_new):
+    count_machines = 0
+    for component_new, values_new in dic_new.items():
+        for component_old, values_old in dic_old.items():
+            if values_new["executionLayer"] == values_old["executionLayer"]  and values_new["Containers"]["container1"]["selectedExecutionResources"] == values_old["Containers"]["container1"]["selectedExecutionResources"]:
+                count_machines += 1
+                values_new["infid"] = values_old["infid"]
+    if count_machines == len(dic_old):
+        machines_same = True
+        print("All the infrastructures are the same")
+    elif count_machines == 0:
+        machines_same = False
+        print("All the infrastructures are different")
+    else:
+        machines_same = False
+        print("The infrastructures are not the same the same")
+    return machines_same
+    
