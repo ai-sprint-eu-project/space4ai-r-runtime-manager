bc084b1ffd99bf9fe8bd9b71e0810699e6e4d927
Giuseppe Caccia <caccia@cefriel.it>
2022-11-24 11:58:00 +0100

    Fix search next and previus component

--- runtime_manager/utils.py
+++ runtime_manager/utils.py
@@ -128,19 +128,15 @@ def searchPreviousComponent(dic, component):
     if ("" == component):
         returnValue = searchLeaf(dic)
     else:
-        root = False
-        clu = dic["System"]["toscas"][component]["topology_template"]["inputs"]["cluster_name"]["default"]
-        for component_new, values_new in dic["System"]["toscas"].items():
-            for k in values_new["topology_template"]["node_templates"]["oscar_service_"+component_new]["properties"]["output"]:
-                # print(" >OUTPUT %s" % (k["storage_provider"]))
-                if ("minio" != k["storage_provider"]):
-                    cluOut = k["storage_provider"].split(".")[1]
-                    if (cluOut == clu):
-                        root = True
+        for k in dic["System"]["toscas"][component]["topology_template"]["node_templates"]["oscar_service_"+component]["properties"]["input"]:
+                for component_new, values_new in dic["System"]["toscas"].items():
+                    for outPath in values_new["topology_template"]["node_templates"]["oscar_service_"+component_new]["properties"]["output"]:
+                        inPath = k["path"]
+                        if outPath["path"] == inPath:
+                            returnValue = component_new
+                            break
+                if ("" != returnValue):
                     break
-            if (True == root):
-                returnValue = component_new
-                break
     return returnValue
 
 def searchNextComponent(dic, component):
@@ -149,37 +145,46 @@ def searchNextComponent(dic, component):
         returnValue = searchRoot(dic)
     else:
         for k in dic["System"]["toscas"][component]["topology_template"]["node_templates"]["oscar_service_"+component]["properties"]["output"]:
-            if ("minio" != k["storage_provider"]):
-                clu = k["storage_provider"].split(".")[1]
+            # if ("minio" != k["storage_provider"]):
+            #     clu = k["storage_provider"].split(".")[1]
 
+            #     for component_new, values_new in dic["System"]["toscas"].items():
+            #         clu_new = values_new["topology_template"]["inputs"]["cluster_name"]["default"]
+            #         if (clu_new == clu):
+            #             returnValue = component_new
+            #             break
+            # else:
                 for component_new, values_new in dic["System"]["toscas"].items():
-                    clu_new = values_new["topology_template"]["inputs"]["cluster_name"]["default"]
-                    if (clu_new == clu):
-                        returnValue = component_new
-                        break
-            if ("" != returnValue):
-                break
+                    for inPath in values_new["topology_template"]["node_templates"]["oscar_service_"+component_new]["properties"]["input"]:
+                        outPath = k["path"]
+                        if inPath["path"] == outPath:
+                            returnValue = component_new
+                            break
+                if ("" != returnValue):
+                    break
     return returnValue
 
 def searchRoot(dic):
     returnValue = ""
     for component_new, values_new in dic["System"]["toscas"].items():
         clu = values_new["topology_template"]["inputs"]["cluster_name"]["default"]
-        root = True
-        for component_new2, values_new2 in dic["System"]["toscas"].items():
-
-            for k in values_new2["topology_template"]["node_templates"]["oscar_service_"+component_new2]["properties"]["output"]:
-                if ("minio" != k["storage_provider"]):
-                    cluOut = k["storage_provider"].split(".")[1]
-                    if (cluOut == clu):
-                        root = False
-                        break
-
-            if (False == root):
-                break
-        if (True == root):
+        if len(values_new["topology_template"]["node_templates"]["oscar_service_"+component_new]["properties"]["input"]) == 1:
             returnValue = component_new
-            break
+        # root = True
+        # for component_new2, values_new2 in dic["System"]["toscas"].items():
+
+        #     for k in values_new2["topology_template"]["node_templates"]["oscar_service_"+component_new2]["properties"]["output"]:
+        #         if ("minio" != k["storage_provider"]):
+        #             cluOut = k["storage_provider"].split(".")[1]
+        #             if (cluOut == clu):
+        #                 root = False
+        #                 break
+
+        #     if (False == root):
+        #         break
+        # if (True == root):
+        #     returnValue = component_new
+        #     break
     return returnValue
 
 def searchRootCluster(fdls):
@@ -650,34 +655,35 @@ def oscar_cli(new_dir, fdls, case):
                 stream = os.popen(command) 
                 output = stream.read()
                 print(output)
-            #     if "successfully stored" in output:
-            #         command = "%s service list  -c %s --config %s " % (oscar_cli, identifier, config_dir)
-            #         print("SERVICE LIST: %s" % command)
-            #         print("\n")
-            #         stream = os.popen(command)
-            #         output = stream.read()
-            #         print(output)
-            #         if "There are no services in the cluster" not in output:
-            #             output_split = output.split("\n")
-            #             for line in output_split:
-            #                 if "NAME" not in line and line != "":
-            #                     service_old = line.split("\t")[0]
-            #                     command = "%s service remove %s -c %s --config %s" % (oscar_cli, service_old, identifier, config_dir)
-            #                     print("SERVICE REMOVE: %s" % command)
-            #                     print("\n")
-            #                     stream = os.popen(command)
-            #                     output = stream.read()
-            #                     print(output)
-            #                     minio_cli(endpoint_minio, access_key_minio, secret_key_minio, service_old, "DELETE")
+                if "successfully stored" in output:
+                    command = "%s service list  -c %s --config %s " % (oscar_cli, identifier, config_dir)
+                    print("SERVICE LIST: %s" % command)
+                    print("\n")
+                    stream = os.popen(command)
+                    output = stream.read()
+                    print(output)
+                    if "There are no services in the cluster" not in output:
+                        output_split = output.split("\n")
+                        for line in output_split:
+                            if "NAME" not in line and line != "":
+                                service_old = line.split("\t")[0]
+                                command = "%s service remove %s -c %s --config %s" % (oscar_cli, service_old, identifier, config_dir)
+                                print("SERVICE REMOVE: %s" % command)
+                                print("\n")
+                                stream = os.popen(command)
+                                output = stream.read()
+                                print(output)
+                                minio_cli(endpoint_minio, access_key_minio, secret_key_minio, service_old, "DELETE")
                 nextFdl = searchNextFdl(fdls, nextFdl)
-            # command = "%s apply %s/fdl-new.yaml --config %s" % (oscar_cli, new_dir, config_dir)
-            # print("APPLY: " + command)
-            # print("\n")
-            # stream = os.popen(command)
-            # output = stream.read()
-            # print(output)
-            # if "Applying file" in output:
-            #     print("FDL is being applied")
+                
+            command = "%s apply %s/fdl-new.yaml --config %s" % (oscar_cli, new_dir, config_dir)
+            print("APPLY: " + command)
+            print("\n")
+            stream = os.popen(command)
+            output = stream.read()
+            print(output)
+            if "Applying file" in output:
+                print("FDL is being applied")
 
     else:
         print("It is not found oscar-cli path")
