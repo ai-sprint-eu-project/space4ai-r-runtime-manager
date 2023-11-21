086892d9dc55342377c6236ded21aa8dabbe940e
root <caccia@cefriel.it>
2023-11-21 09:30:03 +0000

    krake rest API

--- runtime_manager/RM_interface.py
+++ runtime_manager/RM_interface.py
@@ -4,6 +4,9 @@ import requests
 import os
 import sys
 import json
+from werkzeug.utils import secure_filename
+import subprocess
+import logging
 
 
 ###############
@@ -23,8 +26,44 @@ def ka():
     else:
         return jsonify("ERROR", 404)
 
+@app.route("/krake", methods=['POST'])
+def krake():
+    if request.method == 'POST':
+        with open("/mnt/config.json") as c:
+            cfg= json.load(c)
+
+        content_type = request.headers.get('Content-Type') 
+        if  'multipart/form-data' in content_type:
+            f = request.files['file']
+            f.save(secure_filename(f.filename))
+            OPTIMIZER_FOLDER = "/home/SPACE4AI-R/optimiser/"
+            RUNTIMEMANAGER_FOLDER = "/home/SPACE4AI-R/runtime_manager/runtime_manager/"
+            cmd = ['/bin/sh', '-c' , 'cd ' + \
+                                     OPTIMIZER_FOLDER + \
+                                     ' && python3 estimate_CPUs.py ' + \
+                                     RUNTIMEMANAGER_FOLDER + f.filename + ' ' + \
+                                     str(cfg['reconfiguration_time']) + ' ' + \
+                                     str(cfg['total_execution_time_constraint']) + ' ' +\
+                                     '/mnt/' + cfg['profiling_data'] + ' ' +\
+                                     str(cfg['min_cores']) + ' ' + \
+                                     str(cfg['max_cores']) \
+                  ]
+            run_cmd = subprocess.run(cmd, capture_output=True, text=True)
+            out_3 = run_cmd.stdout
+            app.logger.info(cmd)
+            app.logger.info(out_3)
+
+            with open(OPTIMIZER_FOLDER + "/out.json") as c:
+                o= json.load(c)
+        else:
+            return 'Content-Type not supported!' 
+
+        return jsonify("KRAKE", 0, 200)
+    else:
+        return jsonify("ERROR", 404)
+
 @app.route("/ams-alert", methods=['POST'])
-def infrastructures():
+def ams():
     if request.method == 'POST':
         content_type = request.headers.get('Content-Type')
         if (content_type == 'application/json'):
